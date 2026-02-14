e34cd7e736921de09c8fa32bd28ec32bb3fd5be9
mael Bourgeois <mael.bourgeois@edu.devinci.fr>
2026-02-14 14:37:06 +0100

    Add calculation for potential profitability based on VINTED items

--- client/v1/index.js
+++ client/v1/index.js
@@ -499,7 +499,11 @@ const deal = {
 
 // 1. Compute the potential highest profitability based on the VINTED items
 // 2. Log the value
-
+const potentialProfitability = VINTED.reduce((maxProfit, item) => {
+  const profit = parseFloat(item.price) - deal.price;
+  return profit > maxProfit ? profit : maxProfit;
+}, 0);
+console.log(potentialProfitability);
 
 
 /**
