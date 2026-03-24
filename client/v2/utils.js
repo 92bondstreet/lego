af019b32c4f89bbf9d7f4b9f8b639bd1f6ce491a
FnxNickname <ilyass.bachiri@gmail.com>
2026-03-24 15:10:22 +0100

    fix(utils): improve lego set id extraction

--- client/v2/utils.js
+++ client/v2/utils.js
@@ -8,5 +8,6 @@
  * @returns {Array} list of lego set ids
  */
 const getIdsFromDeals = deals => {
-    return deals.map(deal => deal.id)
+    const ids = deals.map(deal => deal.id || (deal.uuid ? deal.uuid.substring(0, 8) : null)).filter(id => id);
+    return [...new Set(ids)];
 }
